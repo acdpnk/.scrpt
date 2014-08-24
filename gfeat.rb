8ab5c4cc7b1905146ba04f5a7d1ae1d0890ee0e2
chrifpa <chrifpa@gmail.com>
2014-08-24 11:42:23 +0200

    use date +%y%m%d%H%M%S instead of uuidgen to provide git-flow scripts with unique identifiers

--- gfeat.rb
+++ gfeat.rb
@@ -1,6 +1,6 @@
 #!/usr/local/bin/ruby
 
-command = "git flow feature start #{ARGV[0]}_#{%x(uuidgen)}"
+command = "git flow feature start #{ARGV[0]}_#{%x(date +%y%m%d%H%M%S)}"
 
 puts command
 puts %x(#{command})
